bccf623ed83a94fd5ffa8dc735b67b26d0f6901d
yessaliyev <40271311+yessaliyev@users.noreply.github.com>
2023-09-04 13:53:18 +0600

    fix linter

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/agrison/go-commons-lang/stringUtils"
 )
 
 func main() {
-	message := "Hello, OTUS!"
-	fmt.Print(stringUtils.Reverse(message))
+	text := "Hello, OTUS!"
+	fmt.Print(stringUtils.Reverse(text))
 }
