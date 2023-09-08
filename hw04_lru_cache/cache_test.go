04586ca09f5cf58aa84030c92319987888f22a2c
yessaliyev <40271311+yessaliyev@users.noreply.github.com>
2023-09-08 17:28:56 +0600

    fix linter

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -67,7 +67,7 @@ func TestCache(t *testing.T) {
 	})
 }
 
-func TestCacheMultithreading(t *testing.T) {
+func TestCacheMultithreading(_ *testing.T) {
 	c := NewCache(10)
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
